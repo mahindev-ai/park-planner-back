9dc05cae5e9a077ef9cca128faa421c3fb794dc1
mahindev-ai <mahindev.ai@gmail.com>
2023-11-26 04:58:34 -0500

    update::api deploy it

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import os
 from app import app
 
-port = int(os.environ.get('PORT', 8080))
-
-if __name__ == '__main__':
-    app.run(threaded=True, host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
